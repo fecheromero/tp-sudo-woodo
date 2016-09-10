f858464cdaa00036185dd2864d3b81be2d63fca9
fecheromero <fecheromero@gmail.com>
2016-09-09 21:11:31 -0300

    mamaaaaaaaa

--- OsadaMaster/src/OsadaMaster.c
+++ OsadaMaster/src/OsadaMaster.c
@@ -17,8 +17,8 @@ typedef struct osada{
 	osada_header header;
 	t_bitarray bitmap;
 	osada_file archivos[2047];
-	int asignaciones[];
-	osada_block_pointer datos[];
+	int* asignaciones;
+	osada_block_pointer* datos;
 }osada;
 osada mapearFyleSystem(t_bitarray* archivo){
 	osada fyleSystem;
@@ -46,11 +46,15 @@ osada mapearFyleSystem(t_bitarray* archivo){
 	printf("%d\n",fyleSystem.header.allocations_table_offset);
 	indice+=4;
 	puts("tamaño de la tabla de datos \n");
-	fyleSystem.header.fs_blocks=atoi(string_substring(contenido,indice,4));
+	fyleSystem.header.data_blocks=atoi(string_substring(contenido,indice,4));
 	indice+=4;
 	printf("%d\n",fyleSystem.header.fs_blocks);
-	fyleSystem.bitmap=atoi(string_substring(contenido,indice,(fyleSystem.header.fs_blocks/8/OSADA_BLOCK_SIZE)));
-	printf("%d\n",fyleSystem.bitmap);
+	float sizeD=fyleSystem.header.fs_blocks/8/64;
+	//fyleSystem.bitmap.size=ceilf();
+	printf("%d",fyleSystem.bitmap.size);
+	fyleSystem.bitmap.bitarray=string_substring(contenido,indice,(fyleSystem.bitmap.size));
+	puts("bitarray \n");
+	printf("%d\n",fyleSystem.bitmap.bitarray);
 	return fyleSystem;
 };
  void rellenarCadena(int size,char* texto,char vector[]){
@@ -59,13 +63,28 @@ osada mapearFyleSystem(t_bitarray* archivo){
 
 int main(void) {
 	t_bitarray* fyleSystem;
-	fyleSystem=bitarray_create("OsadaFS11500330442",96000);
-	puts("por aca paso");
-	mapearFyleSystem(fyleSystem);
-	puts("todo ok");
+	osada unOsada;
+	unOsada.header.fs_blocks=1500;
+	char unChar[7]="unFyleSytem";
+	strcpy(unOsada.header.magic_number,unChar);
+	unOsada.header.allocations_table_offset=30;
+	unOsada.header.version=1;
+	unOsada.header.bitmap_blocks=3;
+	unOsada.header.data_blocks=442;
+	unOsada.datos=NULL;
+	unOsada.asignaciones=NULL;
+	osada otroOsada;
+	fyleSystem=bitarray_create("OsadaFS11500000300300442000000000000000000000000000000000000000",96000);
+	//puts("por aca paso");
+	//mapearFyleSystem(fyleSystem);
+	FILE* saraza=fopen("//home//utnso//texto.bin","wb");
+			fwrite(&unOsada,sizeof(osada),1,saraza);
+			fread(&otroOsada,sizeof(osada),1,saraza);
+		printf("%s\n",otroOsada.header.magic_number);
+		printf("%d\n",otroOsada.header.data_blocks);
 	return EXIT_SUCCESS;
 }
-//tamaño en boques=1500
+//tamaño en bloques=1500
 //1 bloque ->header
 //3 bloques->bitmap
 //1024 bloques->tabla de archivos
