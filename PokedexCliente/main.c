4886e1d6adc0648ee646767a28216bb3c646e351
achus619 <achus619@gmail.com>
2016-10-14 18:22:55 -0300

    Funciones readdir y getattr incompletas. Faltan mensajes con server.

--- PokedexCliente/main.c
+++ PokedexCliente/main.c
@@ -10,44 +10,15 @@
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
 #include <fcntl.h>
-
-
-/* Este es el contenido por defecto que va a contener
- * el unico archivo que se encuentre presente en el FS.
- * Si se modifica la cadena se podra ver reflejado cuando
- * se lea el contenido del archivo
- */
-#define DEFAULT_FILE_CONTENT "Hello World!\n"
-
-/*
- * Este es el nombre del archivo que se va a encontrar dentro de nuestro FS
- */
-#define DEFAULT_FILE_NAME "hello"
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
 
 /*
  * Este es el path de nuestro, relativo al punto de montaje, archivo dentro del FS
  */
 #define DEFAULT_FILE_PATH "/" DEFAULT_FILE_NAME
-
-
-
-/*
- * Esta es una estructura auxiliar utilizada para almacenar parametros
- * que nosotros le pasemos por linea de comando a la funcion principal
- * de FUSE
- */
-struct t_runtime_options {
-	char* welcome_msg;
-} runtime_options;
-
-/*
- * Esta Macro sirve para definir nuestros propios parametros que queremos que
- * FUSE interprete. Esta va a ser utilizada mas abajo para completar el campos
- * welcome_msg de la variable runtime_options
- */
-#define CUSTOM_FUSE_OPT_KEY(t, p, v) { t, offsetof(struct t_runtime_options, p), v }
 
 
 /*
@@ -64,21 +35,28 @@ struct t_runtime_options {
  * 	@RETURN
  * 		O archivo/directorio fue encontrado. -ENOENT archivo/directorio no encontrado
  */
-static int hello_getattr(const char *path, struct stat *stbuf) {
+static int tp_getattr(const char *path, struct stat *stbuf) {
 	int res = 0;
+	int link;
+	int permisos;
+	char* tipo;
 
 	memset(stbuf, 0, sizeof(struct stat));
 
 	//Si path es igual a "/" nos estan pidiendo los atributos del punto de montaje
 
-	if (strcmp(path, "/") == 0) {
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-	} else if (strcmp(path, DEFAULT_FILE_PATH) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(DEFAULT_FILE_CONTENT);
-	} else {
+	enviar() //path + getattr
+	recibir() //tipo de archivo (dir o arch), permisos, link, size
+	//Transformar lo que devuelva el server a los parametros tipo, permisos, link y size que necesito
+
+	if(tipo==S_IFDIR){
+				stbuf->st_mode = tipo | permisos;
+				stbuf->st_nlink = link;
+	}else if (strcmp(path, DEFAULT_FILE_PATH) == 0){
+				stbuf->st_mode = tipo | permisos;
+				stbuf->st_nlink = link;
+				stbuf->st_size = size;
+	}else {
 		res = -ENOENT;
 	}
 	return res;
@@ -101,9 +79,12 @@ static int hello_getattr(const char *path, struct stat *stbuf) {
  * 	@RETURN
  * 		O directorio fue encontrado. -ENOENT directorio no encontrado
  */
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+static int tp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
 	(void) offset;
 	(void) fi;
+
+	enviar() //path + readdir
+	recibir() //
 
 	if (strcmp(path, "/") != 0)
 		return -ENOENT;
@@ -118,100 +99,16 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler, of
 }
 
 /*
- * @DESC
- *  Esta función va a ser llamada cuando a la biblioteca de FUSE le llege un pedido
- * para tratar de abrir un archivo
- *
- * @PARAMETROS
- * 		path - El path es relativo al punto de montaje y es la forma mediante la cual debemos
- * 		       encontrar el archivo o directorio que nos solicitan
- * 		fi - es una estructura que contiene la metadata del archivo indicado en el path
- *
- * 	@RETURN
- * 		O archivo fue encontrado. -EACCES archivo no es accesible
- */
-static int hello_open(const char *path, struct fuse_file_info *fi) {
-	if (strcmp(path, DEFAULT_FILE_PATH) != 0)
-		return -ENOENT;
-
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
-
-	return 0;
-}
-
-/*
- * @DESC
- *  Esta función va a ser llamada cuando a la biblioteca de FUSE le llege un pedido
- * para obtener el contenido de un archivo
- *
- * @PARAMETROS
- * 		path - El path es relativo al punto de montaje y es la forma mediante la cual debemos
- * 		       encontrar el archivo o directorio que nos solicitan
- * 		buf - Este es el buffer donde se va a guardar el contenido solicitado
- * 		size - Nos indica cuanto tenemos que leer
- * 		offset - A partir de que posicion del archivo tenemos que leer
- *
- * 	@RETURN
- * 		Si se usa el parametro direct_io los valores de retorno son 0 si  elarchivo fue encontrado
- * 		o -ENOENT si ocurrio un error. Si el parametro direct_io no esta presente se retorna
- * 		la cantidad de bytes leidos o -ENOENT si ocurrio un error. ( Este comportamiento es igual
- * 		para la funcion write )
- */
-static int hello_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
-	size_t len;
-	(void) fi;
-	if (strcmp(path, DEFAULT_FILE_PATH) != 0)
-		return -ENOENT;
-
-	len = strlen(DEFAULT_FILE_CONTENT);
-	if (offset < len) {
-		if (offset + size > len)
-			size = len - offset;
-		memcpy(buf, DEFAULT_FILE_CONTENT + offset, size);
-	} else
-		size = 0;
-
-	return size;
-}
-
-
-/*
  * Esta es la estructura principal de FUSE con la cual nosotros le decimos a
  * biblioteca que funciones tiene que invocar segun que se le pida a FUSE.
  * Como se observa la estructura contiene punteros a funciones.
  */
 
-static struct fuse_operations hello_oper = {
-		.getattr = hello_getattr,
-		.readdir = hello_readdir,
-		.open = hello_open,
-		.read = hello_read,
+static struct fuse_operations tp_oper = {
+		.getattr = tp_getattr,
+		.readdir = tp_readdir,
 };
 
-
-/** keys for FUSE_OPT_ options */
-enum {
-	KEY_VERSION,
-	KEY_HELP,
-};
-
-
-/*
- * Esta estructura es utilizada para decirle a la biblioteca de FUSE que
- * parametro puede recibir y donde tiene que guardar el valor de estos
- */
-static struct fuse_opt fuse_options[] = {
-		// Este es un parametro definido por nosotros
-		CUSTOM_FUSE_OPT_KEY("--welcome-msg %s", welcome_msg, 0),
-
-		// Estos son parametros por defecto que ya tiene FUSE
-		FUSE_OPT_KEY("-V", KEY_VERSION),
-		FUSE_OPT_KEY("--version", KEY_VERSION),
-		FUSE_OPT_KEY("-h", KEY_HELP),
-		FUSE_OPT_KEY("--help", KEY_HELP),
-		FUSE_OPT_END,
-};
 
 // Dentro de los argumentos que recibe nuestro programa obligatoriamente
 // debe estar el path al directorio donde vamos a montar nuestro FS
@@ -228,15 +125,6 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	// Si se paso el parametro --welcome-msg
-	// el campo welcome_msg deberia tener el
-	// valor pasado
-	if( runtime_options.welcome_msg != NULL ){
-		printf("%s\n", runtime_options.welcome_msg);
-	}
 
-	// Esta es la funcion principal de FUSE, es la que se encarga
-	// de realizar el montaje, comuniscarse con el kernel, delegar todo
-	// en varios threads
-	return fuse_main(args.argc, args.argv, &hello_oper, NULL);
+	return fuse_main(args.argc, args.argv, &tp_oper, NULL);
 }
