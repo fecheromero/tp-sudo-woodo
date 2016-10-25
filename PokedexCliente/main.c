f95c77f4a59a6782b69af18d095d0a7f14db6bd3
leoxs22 <leandro.narosky@gmail.com>
2016-10-24 21:30:03 -0300

    Listar funcionando con el server (falta fuse)

--- PokedexCliente/main.c
+++ PokedexCliente/main.c
@@ -5,26 +5,12 @@
  *  Created on: 1/10/2016
  *      Author: utnso
  */
-#include <commons/string.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <fuse.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <signal.h>
-#include <socketes.h>
-#include <osada.h>
-#define DEST_PORT 4555
-
+#include "main.h"
 
 /*
  * Este es el path de nuestro, relativo al punto de montaje, archivo dentro del FS
  */
 #define DEFAULT_FILE_PATH "/" DEFAULT_FILE_NAME
-
 
 /*
  * @DESC
@@ -41,41 +27,41 @@
  * 		O archivo/directorio fue encontrado. -ENOENT archivo/directorio no encontrado
  */
 /*
-static int tp_getattr(const char *path, struct stat *stbuf) {
-	int res = 0;
-	int link;
-	int permisos;
-	char* tipo;
-	int size;
+ static int tp_getattr(const char *path, struct stat *stbuf) {
+ int res = 0;
+ int link;
+ int permisos;
+ char* tipo;
+ int size;
 
-	memset(stbuf, 0, sizeof(struct stat));
+ memset(stbuf, 0, sizeof(struct stat));
 
-	//Si path es igual a "/" nos estan pidiendo los atributos del punto de montaje
+ //Si path es igual a "/" nos estan pidiendo los atributos del punto de montaje
 
-	enviar(); //path + getattr
+ enviar(); //path + getattr
 
-	//Neceisto: tipo, tamaño, link
-	//envio: path de archivo
+ //Neceisto: tipo, tamaño, link
+ //envio: path de archivo
 
-	recibir(); //tipo de archivo (S_IFDIR/S_IFREG) + tamaño archivo (size) + link (cantidad de carpetas que hay que entrar para llegar al
-	//Transformar lo que devuelva el server a los parametros tipo, permisos, link y size que necesito
+ recibir(); //tipo de archivo (S_IFDIR/S_IFREG) + tamaño archivo (size) + link (cantidad de carpetas que hay que entrar para llegar al
+ //Transformar lo que devuelva el server a los parametros tipo, permisos, link y size que necesito
 
-	if(tipo=="S_IFDIR"){
-		//Le damos los permisos nosotros
-				stbuf->st_mode = tipo | 0755;
-				stbuf->st_nlink = link;
-	}else if(tipo=="S_IFREG"){
-				stbuf->st_mode = tipo | 0755;
-				stbuf->st_nlink = link;
-				stbuf->st_size = size;
-	}else{
-		res = -ENOENT;
-	}
-	return res;
-}
+ if(tipo=="S_IFDIR"){
+ //Le damos los permisos nosotros
+ stbuf->st_mode = tipo | 0755;
+ stbuf->st_nlink = link;
+ }else if(tipo=="S_IFREG"){
+ stbuf->st_mode = tipo | 0755;
+ stbuf->st_nlink = link;
+ stbuf->st_size = size;
+ }else{
+ res = -ENOENT;
+ }
+ return res;
+ }
 
 
-/*
+ /*
  * @DESC
  *  Esta función va a ser llamada cuando a la biblioteca de FUSE le llege un pedido
  * para obtener la lista de archivos o directorios que se encuentra dentro de un directorio
@@ -93,106 +79,101 @@ static int tp_getattr(const char *path, struct stat *stbuf) {
  */
 
 /*
-static int tp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
-	(void) offset;
-	(void) fi;
-	char* nombre;
-	int i=0;
-	char* V[];
-//recibo vector con todos los nombres y itero y voy completando
-	enviar(); //path + readdir
+ static int tp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+ (void) offset;
+ (void) fi;
+ char* nombre;
+ int i=0;
+ char* V[];
+ //recibo vector con todos los nombres y itero y voy completando
+ enviar(); //path + readdir
 
-	//Necesito: Vector, con todos los nombres de los elementos
-	//Envio: path
-
-
-	recibir(); //Vector
-
-	while(V[i] != NULL){
-		filler(buf, V[i], NULL, 0);
-		i++;
-	}
-
-	return 0;
-}
-
-static int tp_read(const char *path, char *buf, size_t size, off_t offset,
-		struct fuse_file_info *fi) {
-	char* archivo;
-
-	enviar(); //Read + size + path
-	recibir(); //Archivo
-
-	//Necesito: Archivo char*
-	//Envio: Path + size
-
-		memcpy(buf,archivo,size);
-		return size;
-}
-
-static int tp_unlink(const char *path)
-{
-   enviar(); //Eliminar + path
-   recibir();//0(bien), -1(error)
-
-   //Necesito: Eliminar un archivo del fs + confirmacion
-   //Envio: Path
+ //Necesito: Vector, con todos los nombres de los elementos
+ //Envio: path
 
 
-   if(recibir() == -1){
-        return -errno;
-   }
+ recibir(); //Vector
 
-    return 0;
-}
+ while(V[i] != NULL){
+ filler(buf, V[i], NULL, 0);
+ i++;
+ }
 
-/*
+ return 0;
+ }
+
+ static int tp_read(const char *path, char *buf, size_t size, off_t offset,
+ struct fuse_file_info *fi) {
+ char* archivo;
+
+ enviar(); //Read + size + path
+ recibir(); //Archivo
+
+ //Necesito: Archivo char*
+ //Envio: Path + size
+
+ memcpy(buf,archivo,size);
+ return size;
+ }
+
+ static int tp_unlink(const char *path)
+ {
+ enviar(); //Eliminar + path
+ recibir();//0(bien), -1(error)
+
+ //Necesito: Eliminar un archivo del fs + confirmacion
+ //Envio: Path
+
+
+ if(recibir() == -1){
+ return -errno;
+ }
+
+ return 0;
+ }
+
+ /*
  * Esta es la estructura principal de FUSE con la cual nosotros le decimos a
  * biblioteca que funciones tiene que invocar segun que se le pida a FUSE.
  * Como se observa la estructura contiene punteros a funciones.
  */
 /*
-static struct fuse_operations funciones = {
-		.getattr = tp_getattr,
-		.readdir = tp_readdir,
-		.read = tp_read,
-		.unlink	= tp_unlink,
-};
-*/
-
-
+ static struct fuse_operations funciones = {
+ .getattr = tp_getattr,
+ .readdir = tp_readdir,
+ .read = tp_read,
+ .unlink	= tp_unlink,
+ };
+ */
 
 int main(int argc, char *argv[]) {
-			int socket1;
-			char* primero;
-			char* segundo;
-			char* ruta;
-			int* size;
-			int* tamanio;
-			int n;
-		    socket1=crearSocket();
-		    conectarSocket(socket1,DEST_PORT,IP_LOCAL);
-		    ruta=calloc(9,sizeof(char));
-		    string_append(&ruta,"Pokemons");
-		    printf("Ruta: %s \n", ruta);
-		    size=calloc(1,sizeof(int));
-		    *size=8;
-	        puts("teclea algo para enviar paquete");
-	        enviar(socket1,size,sizeof(int));
-	        puts("teclea algo para enviar paquete2");
-	        enviar(socket1,ruta,string_length(ruta));
-	        tamanio=calloc(1,sizeof(int));
-	        recibir(socket1,tamanio,sizeof(int));
-	        printf("%d \n",*tamanio);
-	        puts("pase por aqui");
-	        osada_file* vector=calloc(*tamanio,sizeof(osada_file));
-	        recibir(socket1,vector,(*tamanio)*sizeof(osada_file));
-	        puts("pase por aqui 2.0");
-	        free(ruta);
-	        free(size);
-	       for(n=0; n<*tamanio; n++){
-	    	   printf("Elemento numero %d: fname: %s fsize: %d \n",n,(vector[n]).fname,(vector[n]).file_size);
-	       }
-	        //return fuse_main(argc, argv, &funciones, NULL);
-	        return 0;
+	int socket1;
+	char* ruta;
+	int n;
+	int* tamanio = calloc(1, sizeof(int));
+	socket1 = crearSocket();
+	conectarSocket(socket1, DEST_PORT, IP_LOCAL);
+	ruta = calloc(9, sizeof(char));
+	puts("Ingrese una ruta");
+	scanf("%s", ruta);
+	osada_file* vector = listarDirServer(ruta, socket1, tamanio);
+	free(ruta);
+	for (n = 0; n < *tamanio; n++) {
+		printf("Elemento numero %d: fname: %s fsize: %d \n", n,
+				(vector[n]).fname, (vector[n]).file_size);
 	}
+	//return fuse_main(argc, argv, &funciones, NULL);
+	return 0;
+}
+
+osada_file* listarDirServer(char* path, int socket, int* tamanio) {
+	int* size = calloc(1, sizeof(int));
+	*size = string_length(path);
+	enviar(socket, size, sizeof(int));
+	enviar(socket, path, size);
+	recibir(socket, tamanio, sizeof(int));
+	osada_file* files = calloc(*tamanio, sizeof(osada_file));
+	recibir(socket, files, (*tamanio) * sizeof(osada_file));
+
+	return files;
+}
---
+++ PokedexCliente/main.h
@@ -0,0 +1,27 @@
+/*
+ * main.h
+ *
+ *  Created on: 24/10/2016
+ *      Author: utnso
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <commons/string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <socketes.h>
+#include <osada.h>
+#define DEST_PORT 4555
+
+osada_file* listarDirServer(char* path, int socket, int* tamanio);
+
+#endif /* MAIN_H_ */
