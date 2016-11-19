10753f7266bc545cabee628acadbdae4fbec5a68
leoxs22 <leandro.narosky@gmail.com>
2016-11-18 22:45:21 -0300

    Install libraries script

---
+++ install-libraries.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir libraries
+cd libraries
+echo "DESCARGANDO COMMONS"
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+echo "DESCARGANDO NIVEL-GUI"
+git clone https://github.com/sisoputnfrba/so-nivel-gui-library.git
+echo "DESCARGANDO PKMN-UTILS"
+git clone https://github.com/sisoputnfrba/so-pkmn-utils.git
+
+cd so-commons-library
+echo "INSTALANDO COMMONS"
+sudo make install
+cd ..
+
+cd so-pkmn-utils
+cd src
+echo "INSTALANDO PKMN-UTILS"
+make all
+sudo make install
+cd ../..
+
+cd so-nivel-gui-library
+echo "INSTALANDO NIVEL-GUI"
+make && make install
